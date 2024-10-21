d895de0e54a3c3a6790f0007119481203e1af3b1
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-10-21 23:48:08 +0600

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/screens/home_screen.dart';
+import 'package:news_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/splash_pic.jpg',
+            fit: BoxFit.cover,
+            // width: width * .9,
+              height: height * .5,
+            ),
+            SizedBox(height: height * .04,),
+            Text('TOP HEADLINES', style: GoogleFonts.anton(letterSpacing: .6,color: Colors.grey.shade700),),
+            SizedBox(height: height * .04,),
+            SpinKitChasingDots(
+              color: Colors.blue,
+              size: 40,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
